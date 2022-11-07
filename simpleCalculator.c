a36852f54e1b9c52a151609d9c08abb1e9585ce7
Shubham kumar <116937585+Kmrshubham335@users.noreply.github.com>
2022-11-07 20:25:03 +0530

    Add files via upload

---
+++ simpleCalculator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a=4;
+    int b=2;
+    int p;
+    p=a+b;
+    printf("Sum of the two number:=%d\n",p);
+    p=a-b;
+    printf("Diffrence of the two number:=%d\n",p);
+    p=a*b;
+    printf("Product of the two number:=%d\n",p);
+    p=a/b;
+    printf("Quotient of the two number:=%d\n",p);
+    p=a%b;
+    printf("Product of the two number:=%d\n",p);
+    return 0;
+}
