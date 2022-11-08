7eedcb5ec2c49094261d7ad20d0144bdc5f6358a
Shubham kumar <116937585+Kmrshubham335@users.noreply.github.com>
2022-11-08 12:46:59 +0530

    Add files via upload

---
+++ Swap.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=4;
+    int b=2;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    printf("value of a after swaping:=%d\n",a);
+    printf("value of b after swaping:=%d\n",b);
+    return 0;
+
+
+}
